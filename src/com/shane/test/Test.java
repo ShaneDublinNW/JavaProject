d212fec4cda743a57d6dc86bf9af45fd25cffcc1
Shane Dublin <dublis1@nationwide.com>
2018-03-02 11:03:51 -0700

    testing

--- src/com/shane/test/Test.java
+++ src/com/shane/test/Test.java
@@ -1,5 +1,23 @@
 package com.shane.test;
 
 public class Test {
+	
+	public static void main(String[] args) {
+		
+		final String  s1 = "";
+		String s2, s3,s4;
+		
+		String s = "test";
+		switch (s) {
+		case s1:
+			
+			break;
+
+		default:
+			break;
+		}
+		
+		
+	}
 
 }
